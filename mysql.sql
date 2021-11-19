50248b4afaaee4e13bf13cfddd03dbf01ba77f1b
Abderrahmane Fadili <abdouelrfadili@gmail.com>
2021-11-19 15:19:18 +0100

    Create models & tables

---
+++ mysql.sql
@@ -0,0 +1,20 @@
+-- CREATE DATABASE travelia_blog_appDB;
+
+-- USE travelia_blog_appDB;
+
+-- Insert some categories
+
+/*INSERT INTO categories (title) VALUES 
+('Air travel'),
+('Bleisure travel'),
+('Honeymoon');*/
+
+
+-- SELECT * FROM categories;
+-- SELECT * FROM posts;
+-- SELECT * FROM post_likes;
+-- SELECT * FROM post_comments;
+-- SELECT * FROM post_comment_replies;
+
+
+
